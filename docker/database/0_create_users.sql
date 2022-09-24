d4dc99ee4d3f518a0a5e5a33c9d7f93c0c8d07ad
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-09-23 21:36:17 -0300

    Imagem docker configurada

---
+++ docker/database/0_create_users.sql
@@ -0,0 +1,14 @@
+CREATE USER quickstart 
+    WITH 
+    PASSWORD 'quickstart' 
+    CREATEDB;
+
+CREATE USER quickstart_log 
+    WITH 
+    PASSWORD 'quickstart' 
+    CREATEDB;
+
+CREATE USER quickstart_test 
+    WITH 
+    PASSWORD 'quickstart' 
+    CREATEDB;
---
+++ docker/database/1_create_databases.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE quickstart
+    WITH
+    OWNER = quickstart
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE quickstart_log
+    WITH
+    OWNER = quickstart_log
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE quickstart_test
+    WITH
+    OWNER = quickstart_test
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    CONNECTION LIMIT = -1;
